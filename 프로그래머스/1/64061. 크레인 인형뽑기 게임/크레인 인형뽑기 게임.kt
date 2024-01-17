13db59d208437f2df0d6948fd1ef13377b21a228
Cosine-A <100404990+Cosine-A@users.noreply.github.com>
2024-01-17 17:54:36 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 9.07 ms, Memory: 60.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.kt
@@ -0,0 +1,31 @@
+class Solution {
+    
+    private var removed = 0
+    private val picks = mutableListOf<Int>()
+
+    fun solution(boards: Array<IntArray>, moves: IntArray): Int {
+        moves.forEach { line ->
+            val realLine = line - 1
+            for (board in boards) {
+                val target = board[realLine]
+                if (target == 0) continue
+                board[realLine] = 0
+                picks.add(target)
+                addPoint()
+                break
+            }
+        }
+        return removed
+    }
+
+    private fun addPoint() {
+        val size = picks.size
+        val pick = picks.getOrNull(size - 2) ?: return
+        if (pick == picks.last()) {
+            removed += 2
+            repeat(2) {
+                picks.removeLast()
+            }
+        }
+    }
+}
