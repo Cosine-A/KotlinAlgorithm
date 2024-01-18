d8d1924054584d749d33a0f60b70f9ae8906a12b
Cosine-A <100404990+Cosine-A@users.noreply.github.com>
2024-01-18 16:48:00 +0900

    fix: 로직 변경

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.kt
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.kt
@@ -1,4 +1,24 @@
 class Solution {
+    // 새 코드
+    fun solution(input: String): String {
+        var newInput = ""
+        var index = 0
+        for (char in input) {
+            if (char.isWhitespace()) {
+                index = -1
+                newInput += " "
+            } else {
+                newInput += if (index % 2 == 0) {
+                    char.uppercase()
+                } else {
+                    char.lowercase()
+                }
+            }
+            index++
+        }
+        return newInput
+    }
+    /* 이전 코드
     fun solution(input: String): String {
         return input.split(" ").map { str ->
             str.mapIndexed { index, char ->
@@ -10,4 +30,5 @@ class Solution {
             }.joinToString("")
         }.joinToString(" ")
     }
+    */
 }
