d0983e1fa8a658e33d70edaf5cfa6a1a5e643eec
Cosine-A <100404990+Cosine-A@users.noreply.github.com>
2024-03-22 20:30:45 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 12.56 ms, Memory: 61.2 MB -BaekjoonHub

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.kt
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.kt
@@ -2,7 +2,7 @@ class Solution {
     fun solution(phoneNumber: String): String {
         val length = phoneNumber.length
         val star = "*".repeat(length - 4)
-        val number = phoneNumber.replaceRange(0..length - 5, "")
+        val number = phoneNumber.slice(length - 4 until length)
         return star + number
     }
 }
